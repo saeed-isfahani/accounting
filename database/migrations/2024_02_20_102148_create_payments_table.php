9453ea818bcd9efd30d9f9a9de67497aef8ff19f
saeed <saeed.com2000@gmail.com>
2024-02-20 14:22:41 +0330

    feat: add Payment model and migration

---
+++ database/migrations/2024_02_20_102148_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency');
+            $table->double('amount');
+            $table->integer('rate');
+            $table->bigInteger('amount_in_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
